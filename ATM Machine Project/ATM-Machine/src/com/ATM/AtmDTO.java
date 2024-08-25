b38a712d6823576499dcd44506f9a3ed5cf8e102
safran <64087677+msafran25@users.noreply.github.com>
2024-08-25 20:45:27 +0400

    Add files via upload

---
+++ ATM Machine Project/ATM-Machine/src/com/ATM/AtmDTO.java
@@ -0,0 +1,31 @@
+package com.ATM;
+
+public class AtmDTO {
+    private double balance;
+    private double withdrawAmount;
+    private double depositAmount;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
+
+    public void setWithdrawAmount(double withdrawAmount) {
+        this.withdrawAmount = withdrawAmount;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+}
